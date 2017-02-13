aa338b9003c85fe86c24de6f49d3beb05cd06fbe
Tejaswita <tejaswita@hashtaag.com>
2017-02-13 15:45:31 +0530

    Tejaswita

--- src/presentation/Header.js
+++ src/presentation/Header.js
@@ -31,6 +31,59 @@ export default class Header extends React.Component {
 // 		this.setState({ showSignIn: true });
 // 	}
 
+	constructor(props) {
+		super(props);
+		this.checkLoginState = this.checkLoginState.bind(this);
+		this.statusChangeCallback = this.statusChangeCallback.bind(this);
+		window.fbAsyncInit = function() {
+			FB.init({
+				appId      : '1262754500513919',
+				xfbml      : true,
+				version    : 'v2.8'
+			});
+			FB.AppEvents.logPageView();
+		};
+	}
+
+	userLoggedIn() {
+		console.log('Welcome!  Fetching your information.... ');
+		FB.api('/me', function(response) {
+			console.log('Successful login for: ' + response.name);
+			//document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
+		});
+	}
+
+	statusChangeCallback(response) {
+		console.log('statusChangeCallback');
+		console.log(response);
+		// The response object is returned with a status field that lets the
+		// app know the current login status of the person.
+		// Full docs on the response object can be found in the documentation
+		// for FB.getLoginStatus().
+		if (response.status === 'connected') {
+			// Logged into your app and Facebook.
+			console.log("User logged in");
+			this.userLoggedIn();
+		} else if (response.status === 'not_authorized') {
+			console.log("Not authorized");
+			// The person is logged into Facebook, but not your app.
+			//document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
+		} else {
+			console.log("Not logged into facebook");
+			// The person is not logged into Facebook, so we're not sure if
+			// they are logged into this app or not.
+			//document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
+		}
+	}
+
+	// This function is called when someone finishes with the Login
+	// Button.  See the onlogin handler attached to it in the sample
+	// code below.
+	checkLoginState() {
+		FB.getLoginStatus(function(response) {
+			this.statusChangeCallback(response);
+		});
+	}
 
 	render() {
 		return (
@@ -43,7 +96,8 @@ export default class Header extends React.Component {
 							<li><a href="#">About Us</a></li>
 							<li><a href="#">Sign In</a></li>
 							<li><a href="#">Sign Up</a></li>
-							<li className="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="true" data-auto-logout-link="true" style={{marginTop:"12px"}}></li>
+							<li className="fb-login-button" scope="public_profile,email"  data-max-rows="1" data-size="medium" data-show-faces="true" data-auto-logout-link="true" onClick={this.checkLoginState} style={{marginTop:"12px"}} style={{marginTop:"12px"}}></li>
+							{/*<li className="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="true" data-auto-logout-link="true" style={{marginTop:"12px"}}></li>*/}
 							<li className="search-container">
 								<input className="search-input" type="text" name="search" placeholder="Search.."></input>
 								<img src='src/assets/search.png' className="search-image"/>
